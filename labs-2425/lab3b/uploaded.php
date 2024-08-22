39472f5ed2f4d49d33fcac9f4f88a3bfd1f4212b
Emmjc <carinomarcus85@gmail.com>
2024-08-22 23:07:56 +0800

    added pdf file upload feature

--- labs-2425/lab3b/uploaded.php
+++ labs-2425/lab3b/uploaded.php
@@ -3,11 +3,18 @@
 $upload_directory = getcwd() . '/uploads/';
 $relative_path = '/uploads/';
 
+// Ensure the uploads directory exists
+if (!is_dir($upload_directory)) {
+    mkdir($upload_directory, 0777, true);
+}
+
+
 // Handle Text File
 $uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
 $temporary_file = $_FILES['text_file']['tmp_name'];
 
 if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
+    echo '<h3>Uploaded Text File:</h3>';
     $text_file_content = file_get_contents($uploaded_text_file, 'r');
     ?>
     <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
@@ -15,6 +22,21 @@ if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
 } else {
     echo 'Failed to upload file';
 }
+
+// Handle PDF File
+if (isset($_FILES['pdf_file'])) {
+    $uploaded_pdf_file = $upload_directory . basename($_FILES['pdf_file']['name']);
+    $temporary_file = $_FILES['pdf_file']['tmp_name'];
+
+    if (move_uploaded_file($temporary_file, $uploaded_pdf_file)) {
+        // Display the uploaded PDF file
+        echo '<h3>Uploaded PDF File:</h3>';
+        echo '<embed src="' . $relative_path . basename($_FILES['pdf_file']['name']) . '" width="600" height="500" type="application/pdf">';
+    } else {
+        echo 'Failed to upload PDF file';
+    }
+}
+
 
 
 echo '<pre>';
